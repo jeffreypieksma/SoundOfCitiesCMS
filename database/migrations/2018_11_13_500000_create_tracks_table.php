84e84fabdc8c4e15276b681ec1de5ed7146f501e
Jeffrey Pieksma <piek1500@student.nhl.nl>
2019-01-02 12:53:53 +0100

    Updated tracks migration

--- database/migrations/2018_11_13_500000_create_tracks_table.php
+++ database/migrations/2018_11_13_500000_create_tracks_table.php
@@ -15,7 +15,7 @@ class CreateTracksTable extends Migration
     {
         Schema::create('tracks', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('audio_zones_id')->nullable();
+            $table->unsignedInteger('audio_zone_id')->nullable();
             $table->unsignedInteger('collection_id');
             $table->string('audio_url');
             $table->float('length')->nullable();
@@ -23,11 +23,11 @@ class CreateTracksTable extends Migration
             $table->float('fadeoutpoint')->nullable();
             $table->float('volume')->nullable(); 
             $table->smallInteger('loopable')->nullable();
-
+            $table->smallInteger('playonce')->nullable();
         });
 
         Schema::table('tracks', function (Blueprint $table) {
-            $table->foreign('audio_zones_id')->references('id')->on('audio_zones')->onDelete('cascade');
+            $table->foreign('audio_zone_id')->references('id')->on('audio_zones')->onDelete('cascade');
             $table->foreign('collection_id')->references('id')->on('zone_collections')->onDelete('cascade');
         });
     }
