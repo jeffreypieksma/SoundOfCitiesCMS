d4dbf23f54ca2927b471d7ebcb9b20ede2253b45
Jeffrey Pieksma <piek1500@student.nhl.nl>
2019-01-10 14:54:48 +0100

    User migration and seeder upate with admin field

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,12 +12,20 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-        'name' => 'admin',
-        'email' => 'admin@admin.com',
-        'password' => bcrypt('admin'),
-        'remember_token' => str_random(10),
-        'email_verified_at' => now(),
-    ]);
-    factory(App\User::class, 15)->create(); 
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'admin' => 1,
+            'remember_token' => str_random(10),
+            'email_verified_at' => now(),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Yoni',
+            'email' => 'yoni@admin.com',
+            'password' => bcrypt('yoni@collier1'),
+            'remember_token' => str_random(10),
+            'email_verified_at' => now(),
+        ]);
+    //factory(App\User::class, 15)->create(); 
     }
 }
