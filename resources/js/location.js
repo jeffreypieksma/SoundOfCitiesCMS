0cb8726d2b172987a2d64972c194632b2a7e34bf
Jeffrey Pieksma <piek1500@student.nhl.nl>
2019-01-16 14:48:45 +0100

    Location update

--- resources/js/location.js
+++ resources/js/location.js
@@ -1,54 +1,41 @@
-module.exports = {
-    getLocation: function(){
-        return getLocation();
-    },
-    showPosition: function (){
-        return showPosition();
+import axios from 'axios'
+
+export class Location {
+    
+    showPosition(position) {
+        // var lat = position.coords.latitude;
+        // var lon = position.coords.longitude;
+    
+        output.innerHTML="Latitude: " + position.coords.latitude + 
+        "<br>Longitude: " + position.coords.longitude;
+    }
+
+    getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(showPosition);
+        }else{
+            this.showError(navigator.error)
+        }
+    }
+    
+    showError(error) {
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                output.innerHTML = "User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                output.innerHTML = "Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                output.innerHTML = "The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                output.innerHTML = "An unknown error occurred."
+                break;
+        }
     }
 }
 
-var output = document.getElementById("output");
-var btn = document.getElementById("test");
-
-
-$(btn).click(function(){
-    getLocation();
-});
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(showPosition);
-        navigator.geolocation.watchPosition(setCoords);
-    }else{
-        //Error handling 
-    }
-}
-
-function setCoords(position){
-    this.coords = position; 
-}
-
-function showPosition(position) {
-    // var lat = position.coords.latitude;
-    // var lon = position.coords.longitude;
-
-    output.innerHTML="Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
-}
-
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            output.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            output.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            output.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            output.innerHTML = "An unknown error occurred."
-            break;
-    }
+window.onload = function () {  
+    let location = new Location()
 }
