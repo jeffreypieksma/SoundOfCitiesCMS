debadd87692d3b53af4f8d067757a1f4e002e20c
Jeffrey Pieksma <piek1500@student.nhl.nl>
2019-01-24 22:00:50 +0100

    Mobile menu an map responsive fixes

--- resources/views/admin/includes/navigation.blade.php
+++ resources/views/admin/includes/navigation.blade.php
@@ -5,12 +5,14 @@
       
       <ul class="right hide-on-med-and-down">
         {{-- <li  class="{{ $current_route_name == '' ? 'active' : '' }}"><a href="{{ route('dashboard') }}">{{ __('app.dashboard') }}</a></li> --}}
+        
         <li  class="{{ $current_route_name == '' ? 'active' : '' }}"><a href="{{ route('collections') }}">{{ __('app.collections') }}</a></li>
         
         <li>
             <a class="dropdown-trigger" href="#!" data-target="dropdown1">
-            {{ (session('locale')) ? session('locale') : 'en' }}     
-            <i class="material-icons right">arrow_drop_down</i></a>
+                {{ (session('locale')) ? session('locale') : 'en' }}     
+                <i class="material-icons right">arrow_drop_down</i>
+            </a>
         </li>
         <ul id="dropdown1" class="dropdown-content" style="z-index:9999999;">
             <li><a href="/locale/en" class="{{ session('locale') === "en" ? "active" : "" }}">English</a></li>
@@ -37,17 +39,18 @@
             <li><a href=""><b>{{ __('app.welcome') }}: {{Auth::user()->name }} </b></a></li>
             <li class="divider"></li>
         @endauth
-    
-        <li><a href="{{ route('collections') }}">{{ __('collection.title') }}</a></li>
+        
+        <li><a href="{{ route('collections') }}">{{ __('app.collections') }}</a></li>
 
         <li>
-            <a class="dropdown-trigger" href="#!" data-target="dropdown3">{{ session('locale') }}
-            <i class="material-icons right">arrow_drop_down</i></a>
+            <a class="dropdown-trigger" href="#!" data-target="dropdown3">
+                {{ (session('locale')) ? session('locale') : 'en' }}     
+                <i class="material-icons right">arrow_drop_down</i>
+            </a>
         </li>
         <ul id="dropdown3" class="dropdown-content" style="z-index:9999999;">
             <li><a href="/locale/en" class="{{ session('locale') === "en" ? "active" : "" }}">English</a></li>
             <li><a href="/locale/nl" class="{{ session('locale') === "nl" ? "active" : "" }}">Nederlands</a></li>
         </ul>
-
     </ul>
 </nav>
